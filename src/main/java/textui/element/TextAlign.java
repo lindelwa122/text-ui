8b69c236837c41e98948524818b9c791eb0b69b3
Nqabenhle <nqmlabajhb024@student.wethinkcode.co.za>
2025-06-06 07:39:53 +0200

    Add TextAlign

---
+++ src/main/java/textui/element/TextAlign.java
@@ -0,0 +1,22 @@
+package textui.element;
+
+/**
+ * Defines horizontal alignment for text within an element.
+ */
+public enum TextAlign {
+
+    /**
+     * Align text to the left edge.
+     */
+    LEFT,
+
+    /**
+     * Align text to the right edge.
+     */
+    RIGHT,
+
+    /**
+     * Center the text horizontally.
+     */
+    CENTER;
+}
